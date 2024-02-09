d30ba671c2ca2973ab81bcb40d3c1a43857a9677
LeeGregory <35972599+LeeGregory@users.noreply.github.com>
2024-02-09 11:38:12 -0500

    maestro web service

---
+++ Salesforce LUB4C/Salesforce LUB4C/Maestro Web Service/LUB4CService/DataContracts/LUB4CRequest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace Profisee.LUB4CService.DataContracts
+{
+	[DataContract]
+	public class LUB4CRequest
+	{
+		[DataMember]
+		public DataContext DataContext { get; set; }
+
+		[DataMember]
+		public Collection<MatchAttribute> MatchAttributes { get; set; }
+
+		[DataMember]
+		public Collection<string> ReturnAttributes { get; set; }
+	}
+
+	[DataContract]
+	public class DataContext
+	{
+		[DataMember]
+		public string Model { get; set; }
+
+		[DataMember]
+		public string Version { get; set; }
+
+		[DataMember]
+		public string Entity { get; set; }
+
+		[DataMember]
+		public string MatchingStrategy { get; set; }
+	}
+
+	[DataContract]
+	public class MatchAttribute
+	{
+		[DataMember]
+		public string Name { get; set; }
+
+		[DataMember]
+		public string Value { get; set; }
+	}
+}
---
+++ Salesforce LUB4C/Salesforce LUB4C/Maestro Web Service/Service_RESTService/IAccountExtensions.cs
@@ -0,0 +1,32 @@
+using System;
+using Mdm.Profisee_Customer360Service.Contracts.MemberDataContracts; 
+using Profisee.MasterDataMaestro.Services.DataContracts.MasterDataServices;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Collections.ObjectModel;
+using System.Collections.Generic;
+using Mdm.Profisee_Customer360Service.Contracts;
+using Mdm.Profisee_Customer360Service.ServiceLibrary;
+
+namespace Mdm.Profisee_Customer360Service.ServiceREST
+{
+    
+    /// <remarks>
+    /// This interface contains custom operations defined for the <see cref="LookupBeforeCreate"/>.
+	/// The implementation of this interface should be created in the AccountExtensions file
+    /// in the same directory as this file.
+    /// </remarks>
+    [ServiceContract()]
+    public interface IAccountExtensions
+    {
+        // TODO: Add any custom service operation you need here. Here's a sample!
+        [WebInvoke(Method = "GET", UriTemplate = "/HelloWorld/{name}")]
+        [OperationContract]
+        String HelloWorld(string name);
+
+	    [WebInvoke(Method = "POST", UriTemplate = "/LookupBeforeCreate")]
+	    [OperationContract]
+	    AccountLUB4CResponse LookupBeforeCreate(AccountLUB4CRequest request);
+    }
+}
+
